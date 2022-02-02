9baa5f09e1ac16be6036dc51edb48ef99d32f394
Asif <1421015@iub.edu.bd>
2022-02-02 14:43:00 +0600

    [in progress] ui of home

---
+++ src/sample/formulae.js
@@ -0,0 +1,48 @@
+const dummyFormulaList= [
+    {
+      id: "61629f4b6fe1fd196ce18c0e",
+      title:"Quadratic formula".toLowerCase(),
+      html: "<p>`x= -b +- (sqrt(b^2 -4ac))/(2a)`</p> <p>`x= -b +- (sqrt(b^2 -4ac))/(2a^2)`</p> <p>&nbsp;</p> <p>&nbsp;</p>  <p>&nbsp;</p>",
+      content: "x= -b +- (sqrt(b^2 -4ac))/(2a^2)"
+    },
+    {
+      id: "61629f4b6fe1fd196ce18c0e",
+      title:"Diiferentiaotion".toLowerCase(),
+      html: "<p>`x= -b +- (sqrt(b^2 -4ac))/(2a)`</p> <p>`x= -b +- (sqrt(b^2 -4ac))/(2a^2)`</p> <p>&nbsp;</p> <p>&nbsp;</p>  <p>&nbsp;</p>",
+      content: "x= -b +- (sqrt(b^2 -4ac))/(2a^2)"
+    },
+    {
+      id: "61629f4b6fe1fd196ce18c0e",
+      title:"Integration".toLowerCase(),
+      html: "<p>`x= -b +- (sqrt(b^2 -4ac))/(2a)`</p> <p>`x= -b +- (sqrt(b^2 -4ac))/(2a^2)`</p> <p>&nbsp;</p> <p>&nbsp;</p>  <p>&nbsp;</p>",
+      content: "x= -b +- (sqrt(b^2 -4ac))/(2a^2)"
+    },
+    {
+      id: "61629f4b6fe1fd196ce18c0e",
+      title:"Binomial".toLowerCase(),
+      html: "<p>`x= -b +- (sqrt(b^2 -4ac))/(2a)`</p> <p>`x= -b +- (sqrt(b^2 -4ac))/(2a^2)`</p> <p>&nbsp;</p> <p>&nbsp;</p>  <p>&nbsp;</p>",
+      content: "x= -b +- (sqrt(b^2 -4ac))/(2a^2)"
+    },
+    {
+      id: "61629f4b6fe1fd196ce18c0e",
+      title:"Arithmetic Progression".toLowerCase(),
+      html: "<p>`x= -b +- (sqrt(b^2 -4ac))/(2a)`</p> <p>`x= -b +- (sqrt(b^2 -4ac))/(2a^2)`</p> <p>&nbsp;</p> <p>&nbsp;</p>  <p>&nbsp;</p>",
+      content: "x= -b +- (sqrt(b^2 -4ac))/(2a^2)"
+    },
+    {
+      id: "61629f4b6fe1fd196ce18c0e",
+      title:"Geometric Progress".toLowerCase(),
+      html: "<p>`x= -b +- (sqrt(b^2 -4ac))/(2a)`</p> <p>`x= -b +- (sqrt(b^2 -4ac))/(2a^2)`</p> <p>&nbsp;</p> <p>&nbsp;</p>  <p>&nbsp;</p>",
+      content: "x= -b +- (sqrt(b^2 -4ac))/(2a^2)"
+    },
+    {
+      id: "61629f4b6fe1fd196ce18c0e",
+      title:"Poisson Distribution".toLowerCase(),
+      html: "<p>`x= -b +- (sqrt(b^2 -4ac))/(2a)`</p> <p>`x= -b +- (sqrt(b^2 -4ac))/(2a^2)`</p> <p>&nbsp;</p> <p>&nbsp;</p>  <p>&nbsp;</p>",
+      content: "x= -b +- (sqrt(b^2 -4ac))/(2a^2)"
+    },
+]
+
+export {
+    dummyFormulaList
+}
--- src/tab/component/Home.js
+++ src/tab/component/Home.js
@@ -1,10 +1,124 @@
-function Home() {
-    return (
-      <div>
-            Home Component
-      </div>
+
+import React, { useEffect, useState } from 'react'
+import Button from '@material-ui/core/Button'
+import MathJax from 'react-mathjax-preview'
+import AutoSuggest from "react-autosuggest";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+//import { listFormula } from './api-formula'
+import { makeStyles } from '@material-ui/styles';
+import { dummyFormulaList } from '../../sample/formulae';
+
+export default function App  () {
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [formulaList, setformulaList] = useState([])
+
+  useEffect(() => {
+    async function fetchFormulaList() {
+    //  const response = await listFormula()
+    const response = [...dummyFormulaList]
+      console.log("List of Formula ", response)
+      setformulaList(response)
+      setSuggestions(response)
+    }
+    fetchFormulaList()
+  }, [])
+
+
+  function getSuggestions(value) {
+    const userInputLength= value.length
+    let lowerCasedSuggestions= formulaList.map(company => {
+      return {
+        id: company.id,
+        title: company.title.toLowerCase(),
+        html: company.html,
+        content: company.content
+      };
+    });
+    return lowerCasedSuggestions.filter(company =>
+      company.title.substr(0, userInputLength).trim()===value.trim().toLowerCase()
     );
   }
+
+  /*
   
-  export default Home;
-  
+    if (user.userName.substr(0, userInputLength).toLowerCase().trim() === formattedUserInput) {
+      return true
+    } else {
+      return false
+    }
+  */
+  const renderSuggestion =(suggestion)=> {
+    return (
+      <>
+      <span>{suggestion.title} - <MathJax math={suggestion.html} /></span>
+      <CopyToClipboard text={suggestion.content}
+                onCopy={() =>{console.log("copied in clipboard")} }>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              style={{backgroundColor:'#00FA9A', fontWeight:'bold', marginLeft:50}}
+            >
+              Copy to Clipboard
+            </Button>
+            </CopyToClipboard>
+      </>
+    );
+  }
+  const handleCLear=()=>{
+    setValue('')
+  }
+  const theme = useStyles();
+  return (
+    <div style={{display:'inline'}}>
+       <Button onClick={handleCLear}>Clear</Button>
+      <AutoSuggest
+        suggestions={suggestions}
+        onSuggestionsClearRequested={() => setSuggestions([])}
+        onSuggestionsFetchRequested={({ value }) => {
+          console.log(value);
+          setValue(value);
+          setSuggestions(getSuggestions(value));
+        }}
+        onSuggestionSelected={(_, { suggestionValue }) =>
+          console.log("Selected: " + suggestionValue)
+        }
+        getSuggestionValue={suggestion => suggestion.title}
+        renderSuggestion={renderSuggestion}
+        inputProps={{
+          placeholder: "Type formula title",
+          value: value,
+          onChange: (_, { newValue, method }) => {
+            setValue(newValue);
+          }
+        }}
+        highlightFirstSuggestion={true}
+        theme={theme}
+
+      />
+     
+    </div>
+  );
+};
+
+const useStyles = makeStyles({
+  container: {
+    position: "relative",
+  },
+  input: {
+    width: "240px",
+    height: "60px",
+    width: '80%',
+    padding: "10px 20px",
+    fontFamily: "Helvetica, sans-serif",
+    fontWeight: "bold",
+    fontSize: "16px",
+    border: "1px solid #aaa",
+    borderRadius: "4px"
+  },
+  inputFocused: {
+    outlineStyle: "none"
+  }
+  // add other styling here...
+});
