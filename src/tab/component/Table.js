642822fc2833eb1cd00992aeced9ef231f988053
Asif <1421015@iub.edu.bd>
2022-02-03 14:28:23 +0600

    [done] edit formula in realtime

--- src/tab/component/Table.js
+++ src/tab/component/Table.js
@@ -1,20 +1,24 @@
 import React, {useState,useEffect} from 'react'
 
-import {Button,Typography,Paper,TableRow,TableHead,TableContainer,TableCell,TableBody,Table} from '@material-ui/core'
+import {Button,Typography,Paper,TableRow,TableHead,TableContainer,TableCell,TableBody,Table,TextField} from '@material-ui/core'
 
 import { sammpleFormula } from '../../sample/formulae';
 import MathJax from 'react-mathjax-preview'
 
 export default function BasicTable() {
   const [rows,setRows]= useState(sammpleFormula)
-const [pageNumber,setPageNumber]= useState(1)
-const [showNextButton,setShowNextButton]= useState(false)
 
-
-
- const clickNextBtn=(value)=>{
-  setPageNumber((current)=> current+value)
+ const handleChange=(_id,value)=>{
+   console.log(_id, value);
+   var indexToModify = rows.findIndex(i => i._id === _id);
+    let copyRows=[...rows]
+    if (indexToModify !== -1) 
+    {
+        copyRows[indexToModify]['content'] = value;
+    }
+   setRows(copyRows)
  }
+
   return (
     <TableContainer component={Paper} >
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -30,13 +34,16 @@ const [showNextButton,setShowNextButton]= useState(false)
         <TableBody>
           {rows.map((row,index) => (
             <TableRow
-              key={row.email}
+              key={row._id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {row._id}
               </TableCell>
-              <TableCell align="right">{row.content}</TableCell>
+              <TableCell align="right">
+                  <TextField multiline={true} style={{width:500}} value=  {row.content}  onChange={(e)=>{handleChange(row._id,e.target.value)}}/>
+                 
+                  </TableCell>
               <TableCell align="right">
                   <span> <MathJax math={row.content} /></span>
                   </TableCell>
