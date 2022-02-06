972795d4f488772cf25504dbcdb87ce4fd96f2aa
Asif <1421015@iub.edu.bd>
2022-02-06 14:41:40 +0600

    [done] find and replace charachter

--- src/tab/component/SubstituteLetter.js
+++ src/tab/component/SubstituteLetter.js
@@ -4,14 +4,46 @@ import { Button, Typography, Paper, TableRow, TableHead, TableContainer, TableCe
 
 import { sammpleFormula, dummyFormulaList } from '../../sample/formulae';
 import { findUnique, replaceAll, doWhichKey, countString } from '../../helper/helper'
+import MathJax from 'react-mathjax-preview'
 
 export default function SubstituteLetter({ findAndReplaceMode, formula }) {
-    const uniqueLetter = findUnique(formula)
-    console.log("unique Letter ", uniqueLetter);
+    const uniqueLetter = findUnique(formula).map((item)=>{
+        return {
+            currentValue: item,
+            newValue:item
+        }
+    })
+
+    const [rows, setRows] = useState(uniqueLetter)
+    const [updatedFormula, setUpdatedFormula] = useState(formula)
+    const handleChange = (currentValue, newValue) => {
+        console.log(currentValue, newValue);
+    
+    
+        var indexToModify = rows.findIndex(i => i.currentValue === currentValue);
+        let copyRows = [...rows]
+        if (indexToModify !== -1) {
+          copyRows[indexToModify]['newValue'] = newValue;
+        }
+        // const value= copyRows.map((item)=> item.updatedFormula).join("");
+        // console.log("Value is ",value);
+        const temp = replaceAll(formula,currentValue,newValue)
+          console.log("temp is ",temp);
+        setRows(copyRows)
+        setUpdatedFormula(temp)
+    
+    
+      }
+
+  
+
     return (
         <Fragment>
             {
                 findAndReplaceMode === true ?
+                  <>
+                     <Typography style={{textAlign:'center'}}> Before  <MathJax math={"`" + formula + "`"} /></Typography>
+                     <Typography style={{textAlign:'center'}}> After  <MathJax math={"`" + updatedFormula + "`"} /></Typography>
 
                     <TableContainer component={Paper} >
                         <Table sx={{ minWidth: 50 }} aria-label="simple table">
@@ -24,21 +56,21 @@ export default function SubstituteLetter({ findAndReplaceMode, formula }) {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {uniqueLetter.map((row, index) => (
+                                {rows.map((row, index) => (
                                     <TableRow
-                                        key={row._id}
+                                        key={row}
                                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                     >
                                         <TableCell component="th" scope="row">
-                                            {row}
+                                            {row.currentValue}
                                         </TableCell>
                                         <TableCell align="right">
-                                            <TextField multiline={true} style={{ width: 500 }} value={row} />
+                                            <TextField multiline={true} style={{ width: 500 }} value={row.newValue} onChange={(e)=>handleChange(row.currentValue,e.target.value )} />
 
 
                                         </TableCell>
                                         <TableCell align="right">
-                                            <span> {formula}</span>
+                                            <span> <MathJax math={"`" + updatedFormula + "`"} /></span>
                                         </TableCell>
 
                                     </TableRow>
@@ -47,7 +79,7 @@ export default function SubstituteLetter({ findAndReplaceMode, formula }) {
                         </Table>
 
                     </TableContainer>
-
+                    </>
                     :
                     <Typography>Preview Mode</Typography>
             }
